9a8cf6e3efa418cc876f4ac63cd1702718ac7164
Adriano <adrianocavalheiro1@gmail.com>
2019-01-04 20:25:51 -0200

    criada view cadastro usuario

---
+++ app/views/usuario/cadastro.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <h3>Cadastro de Usuário</h3>
+             <form action="/usuario/salvar" method="post" id="form_cadastro">
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control"  name="nome" placeholder="Seu nome" value="" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" class="form-control" name="email" placeholder="" value=""" required>
+                </div>
+
+                <button type="submit" class="btn btn-success btn-sm">Salvar</button>
+            </form>
+        </div>
+        <div class=" col-md-3"></div>
+    </div>
+</div>
